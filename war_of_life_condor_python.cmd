bc5c62ae2c72ea7cda67ffb78b84c52488a83e3c
Thomas Rooney <thomas@rooney.co>
2013-03-06 18:02:15 +0000

    added python condor script

---
+++ war_of_life_condor_python.cmd
@@ -0,0 +1,12 @@
+universe        = vanilla
+executable      = /usr/bin/python
+ 
+output          = /dev/null
+input           = /dev/null
+error           = /dev/null
+ 
+log             = /dev/null
+ 
+arguments       = "/vol/bitbucket/tr111/Prolog-War-of-Life/condor_prolog_multiprocess.py"
+
+queue 1
